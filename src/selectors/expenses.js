61ad749752acacc68c4305e7335c364b5b2ed365
Michael Mann <michaelmann0149@gmail.com>
2019-02-22 08:40:40 -0600

    Added expenses summary component.

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 // get visible expenses
-export default (expenses, { text, sortBy, startDate, endDate}) => {
+export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
         const createdAtMoment = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
