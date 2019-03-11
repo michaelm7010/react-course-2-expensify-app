ffb328a777c4688852da63dfea3d0791c27f8abc
Michael Mann <michaelmann0149@gmail.com>
2019-03-11 16:50:05 -0500

    Set up fetch of expense data

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
+import { startSetExpenses} from './actions/expenses';
+
 // import './playground/promises';
 
 const store = configureStore();
@@ -17,4 +19,7 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
